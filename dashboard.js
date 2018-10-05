fe1f25bdd2e5a400f71414176fa2cddd1794e7d5
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-05 19:07:41 -0400

    food images

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,5 @@
 $(document).on('click', '#menu .btn-primary', function(){
     $('.modal-title').text($(this).prev().prev().html());
     $('.modal-body p').text($(this).prev().html())
+    $('.modal img').attr('src',$(this).parent().prev().attr('src'));
 });
