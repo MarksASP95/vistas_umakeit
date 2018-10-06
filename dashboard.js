54a424bb0fbb227850be76c37c8aa2fa2274d8d7
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-06 03:21:40 -0400

    display toggle battle has begun

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,18 @@
-$(document).on('click', '#menu .btn-primary', function(){
-    $('.modal-title').text($(this).prev().prev().html());
-    $('.modal-body p').text($(this).prev().html())
-    $('.modal img').attr('src',$(this).parent().prev().attr('src'));
+$(document).ready(function(){
+    $(document).on('click', '#menu .btn-primary', function(){
+        $('.modal-title').text($(this).prev().prev().html());
+        $('.modal-body p').text($(this).prev().html())
+        $('.modal img').attr('src',$(this).parent().prev().attr('src'));
+    });
+    
+    $('#up-bar span').on('click', function() {
+        if($('#opc-user').css('opacity') == 1){
+            console.log(1);
+            $('#opc-user').animate({opacity: 1}, 1000);
+        }
+        else{
+            console.log(0);
+            $('#opc-user').animate({opacity: 1}, 1000);
+        }
+    });
 });
