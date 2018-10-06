a4eb50ff52fc0b5440f92844220dece0ed5591c5
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-06 14:23:04 -0400

    code cleanup

--- dashboard.js
+++ dashboard.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
     $(document).on('click', '#menu .btn-primary', function(){
-        $('.modal-title').text($(this).prev().prev().html());
-        $('.modal-body p').text($(this).prev().html())
-        $('.modal img').attr('src',$(this).parent().prev().attr('src'));
+
+        let imgSrc = $(this).parent().prev().attr('src');
+        let foodTitle = $(this).prevAll(':eq(1)').html();
+        let foodDesc = $(this).prev().html();
+
+        $('.modal img').attr('src', imgSrc);
+        $('.modal-title').text(foodTitle);
+        $('.modal-body p').text(foodDesc);
     });
     
     $('#up-bar span').on('click', function() {
-        if($('#opc-user').css('opacity') == 1){
-            $('#opc-user').css({opacity: 0});
-        }
-        else{
-            $('#opc-user').css({opacity: 1});
-        }
+
+        let opcUser = $('#opc-user');
+
+        opcUser.css('opacity') == 1 ? opcUser.css({opacity: 0}) : opcUser.css({opacity: 1});
+
     });
 });
