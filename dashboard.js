92bc6170ce109ad5b6430b5bc7e785d2330426e3
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-06 20:17:53 -0400

    cart product deletion

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
     $(document).on('click', '#menu .btn-primary', function(){
 
-        let imgSrc = $(this).parent().prev().attr('src');
-        let foodTitle = $(this).prevAll(':eq(1)').html();
-        let foodDesc = $(this).prev().html();
+        let imgSrc = $(this).parents('.card').children('.card-img-top').attr('src');
+        let foodTitle = $(this).parents('.card-body').children('.card-title').html();
+        let foodDesc = $(this).parents('.card-body').children('.card-text').html();
 
         $('.modal img').attr('src', imgSrc);
         $('.modal-title').text(foodTitle);
         $('.modal-body p').text(foodDesc);
     });
+
+    $(document).on('click', '#menu .btn-danger', function(){
+        $(this).parents('.card').fadeOut();
+    })
     
     $('#up-bar span').on('click', function() {
 
